607adb378ceb944292b58ebb25a813fa70ca6e8f
Sarah F <sarahelayna@gmail.com>
2018-01-28 23:11:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay (musicians, instruments) {
   var beatlesArray = [];
-  for (i = 0; i < musicians.length; i++) {
+  for (var i = 0; i < musicians.length; i++) {
     beatlesArray.push(musicians[1] + " plays " + instruments[1])
   }
   return beatlesArray;
